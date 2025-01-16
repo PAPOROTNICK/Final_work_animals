073a8dd2fd09b24b3ae521a92bcd13899e8d2c62
PAPOROTNICK <reker3014@gmail.com>
2025-01-16 22:50:29 +0500

    заполнение класса Cat, создание связи с классом Pet

--- AnimalRegisrty/src/animalregistry/Cat.java
+++ AnimalRegisrty/src/animalregistry/Cat.java
@@ -1,4 +1,12 @@
 package animalregistry;
 
-public class Cat {
+public class Cat extends Pet {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getSpecificType() {
+        return "Кошка";
+    }
 }
