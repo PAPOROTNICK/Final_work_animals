dc22392a16cb61fe5bf9d03b41b8aeea5f2e0ec1
PAPOROTNICK <reker3014@gmail.com>
2025-01-16 22:49:59 +0500

    заполнение класса Dog, создание связи с классом Pet

--- AnimalRegisrty/src/animalregistry/Dog.java
+++ AnimalRegisrty/src/animalregistry/Dog.java
@@ -1,4 +1,12 @@
 package animalregistry;
 
-public class Dog {
+public class Dog extends Pet {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getSpecificType() {
+        return "Собака";
+    }
 }
