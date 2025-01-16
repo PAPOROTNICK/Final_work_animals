a207658661ce26304e37e64b3206eff90bf03023
PAPOROTNICK <reker3014@gmail.com>
2025-01-16 22:48:04 +0500

    заполнение класса Donkey, создание связи с классом PackAnimal

--- AnimalRegisrty/src/animalregistry/Donkey.java
+++ AnimalRegisrty/src/animalregistry/Donkey.java
@@ -1,4 +1,12 @@
 package animalregistry;
 
-public class Donkey {
+public class Donkey extends PackAnimal {
+    public Donkey(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getSpecificType() {
+        return "Осел";
+    }
 }
