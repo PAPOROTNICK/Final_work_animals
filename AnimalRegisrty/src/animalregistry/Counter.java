1f51feec0d4da68fa85877ff80e08e65656096de
PAPOROTNICK <reker3014@gmail.com>
2025-01-16 22:54:39 +0500

    создание метода add, исключения и т.д.(заполнение counter)

--- AnimalRegisrty/src/animalregistry/Counter.java
+++ AnimalRegisrty/src/animalregistry/Counter.java
@@ -1,4 +1,33 @@
 package animalregistry;
 
-public class Counter {
+public class Counter implements AutoCloseable {
+    private int count = 0;
+    private boolean closed = false;
+    private boolean usedInTry = false;
+
+    public void markUsedInTry() {
+        usedInTry = true;
+    }
+
+    public void add() {
+        if (closed) {
+            throw new IllegalStateException("Ресурс закрыт.");
+        }
+        if (!usedInTry) {
+            throw new IllegalStateException("Счётчик должен использоваться внутри блока try-with-resources.");
+        }
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void close() {
+        if (!usedInTry) {
+            throw new IllegalStateException("Счётчик не был использован внутри блока try-with-resources.");
+        }
+        closed = true;
+    }
 }
