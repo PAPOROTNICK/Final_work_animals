e845e8b56d30f2d3e5cf8ca8c1a51f012d5996bd
PAPOROTNICK <reker3014@gmail.com>
2025-01-16 22:49:23 +0500

    заполнение класса Hamster, создание связи с классом Pet

--- AnimalRegisrty/src/animalregistry/Hamster.java
+++ AnimalRegisrty/src/animalregistry/Hamster.java
@@ -1,4 +1,12 @@
 package animalregistry;
 
-public class Hamster {
+public class Hamster extends Pet {
+    public Hamster(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getSpecificType() {
+        return "Хомяк";
+    }
 }
